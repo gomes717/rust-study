24024982554ab7cae736770252548ed26c17ea7b
Guilherme Gomes <barbozaguilherme717@gmail.com>
2022-03-08 08:09:13 -0300

    First day 30 min

---
+++ main.rs
@@ -0,0 +1,17 @@
+
+fn main()
+{
+    print!("{} {}\n", "Hello", "World");
+    print!("Linha 1\nLinha 2\nLinha 3\n");
+    println!("linha 4");
+    println!("Number: {}", 120);
+    //comments like C/C++
+    /* More comments */
+    println!("sum: {} + {} = {}",10, 20, 10+20);
+    println!("math: {}", (10 - 2)*5%3*(12 + 5)/3);
+    println!("math fp: {}", (10. - 2.)*5.%3.*(12. + 5.)/3.);
+    print!("More \
+            than \
+            one \
+            line");
+}
